f1268fb02e9c96a96a79cc146bc0185f4ad8abd2
arif-hossain <manabethreeplusone@gmail.com>
2022-10-12 20:37:32 +0600

    fifth commit

---
+++ src/components/QuizQuestions/QuizQuestions.js
@@ -0,0 +1,28 @@
+import { useLoaderData } from 'react-router-dom';
+import QuizAnswer from '../QuizAnswer/QuizAnswer';
+
+
+const QuizQuestions = () => {
+
+
+    const quiz = useLoaderData();
+    // console.log(quiz.data.questions);
+    return (
+        <div className=' text-center'>
+            <div className=' bg-slate-50'>
+                <h1 className='text-4xl font-bold mt-16 py-8 '> Quiz of  {quiz.data.name}</h1>
+                <h1 className='text-4xl font-bold '> Total : {quiz.data.total}</h1>
+            </div>
+            {
+                quiz.data.questions.map(questions =>
+
+                    <QuizAnswer
+                        key={questions.id} questions={questions}
+                    ></QuizAnswer>)
+            }
+
+        </div>
+    );
+};
+
+export default QuizQuestions;
---
+++ src/components/Root/Root.js
@@ -0,0 +1,23 @@
+import Header from '../Header/Header'
+
+import { Outlet, useLoaderData } from 'react-router-dom'
+import { createContext } from 'react'
+// import Fixheader from '../FixHeader/Fixheader'
+
+export const CartData = createContext([])
+
+const Root = () => {
+    const card = useLoaderData()
+
+    return (
+
+        <div className="bg-gray-100">
+            <CartData.Provider value={card}>
+                <Header />
+                <Outlet />
+            </CartData.Provider>
+        </div>
+    )
+}
+
+export default Root
