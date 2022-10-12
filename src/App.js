30b2318e1034a15ade6cbee39536de77a96d893e
arif-hossain <manabethreeplusone@gmail.com>
2022-10-12 20:27:39 +0600

    third commit

--- src/App.js
+++ src/App.js
@@ -1,46 +1,9 @@
-
-
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import './App.css';
-import Home from './components/Home/Home';
-import Blog from './components/Home/Statistics/Blog/Blog';
-import Statistics from './components/Home/Statistics/Statistics';
-
-import Main from './Layouts/Main';
+import React from 'react'
+import { RouterProvider } from 'react-router-dom'
+import router from './Utilities/routes'
 
 function App() {
-  const router = createBrowserRouter([
-    {
-      path: '/',
-      element: <Main></Main>,
-      children: [
-        {
-          path: '/',
-          loader: () => fetch('https://openapi.programming-hero.com/api/quiz'),
-
-          element: <Home></Home>
-        },
-        {
-          path: 'statistics',
-          element: <Statistics></Statistics>
-        },
-        {
-          path: 'blog',
-          element: <Blog></Blog>
-        }
-      ]
-    }
-  ])
-  return (
-    <div className="App">
-
-      <RouterProvider router={router}
-
-      >
-
-      </RouterProvider>
-    </div>
-  );
+  return <RouterProvider router={router} />
 }
 
-export default App;
+export default App
---
+++ src/Quiz/Quiz.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { useLoaderData } from 'react-router-dom';
+
+import QuizCard from '../QuizCard/QuizCard';
+
+const Quiz = () => {
+    const quiz = useLoaderData();
+
+    return (
+        <div className='grid lg:grid-cols-4 md:grid-cols-2 lg:ml-5 lg:mr-5 lg:mt-16 '>
+            {
+                quiz.data.map(course => <QuizCard
+                    course={course} key={course.id}
+                ></QuizCard>)
+            }
+        </div>
+    );
+};
+
+export default Quiz;
